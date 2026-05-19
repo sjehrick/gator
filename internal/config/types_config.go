6752ce175530eefc347379468e6d6bd5e0f6cb4d
samuel <samuel.ehrick@gmail.com>
2026-05-19 16:04:30 -0400

    added package comment to config package and added first two fields to the Config struct

--- internal/config/types_config.go
+++ internal/config/types_config.go
@@ -1,3 +1,7 @@
+// Package config provides config structs and functions for the main package
 package config
 
-type Config struct{}
+type Config struct {
+	dbURL           string `json:"db_url"`
+	currentUserName string `json:"current_user_name"`
+}
